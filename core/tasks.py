03c8c1348fcb2e0e9a700f83e9f4cbda7706600f
Abderahman <abdelrahman16174@gmail.com>
2025-10-21 19:55:53 +0300

    Some Improvements

--- core/tasks.py
+++ core/tasks.py
@@ -1,33 +1,31 @@
 from celery import shared_task
-from django.utils import timezone
+from django.conf import settings
+from datetime import date
+from django.core.mail import EmailMessage
 from core.models import Property, Schedule
-from django.core.mail import send_mail
 
 
 @shared_task
 def update_property_availability():
-    today = timezone.now().date()
+    today = date.today()
     Property.objects.filter(available_at__lte=today, available=False).update(
         available=True
     )
     Property.objects.filter(available_at__gte=today, available=True).update(
         available=False
     )
-    Schedule.objects.filter(schedule_date__gt=today, status="Pending").update(
+    Schedule.objects.filter(schedule_date__lt=today, status="Pending").update(
         status="Refused"
     )
-
-    return None
 
 
 @shared_task
 def sendemails_to_users(subject, message, from_email, recipient_list):
-    send_mail(
+    email = EmailMessage(
         subject=subject,
-        message=message,
-        from_email=from_email,
-        recipient_list=[recipient_list],
-        fail_silently=False,
+        body=message,
+        from_email=settings.EMAIL_HOST_USER,
+        to=[recipient_list],
+        reply_to=[from_email],
     )
-
-    return None
+    email.send()
--- home_space/celery.py
+++ home_space/celery.py
@@ -8,6 +8,8 @@ app = Celery("home_space")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
+app.conf.timezone = "Africa/Cairo"
+
 app.conf.beat_schedule = {
     "update-property-availability": {
         "task": "core.tasks.update_property_availability",
